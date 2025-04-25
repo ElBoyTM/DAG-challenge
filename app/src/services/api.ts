8bdbf628b8fb1af936512ab8e146d7fa85c84656
ElBoyTM <nszalenski@gmail.com>
2025-04-25 15:27:16 -0400

    set up api.ts

--- app/src/services/api.ts
+++ app/src/services/api.ts
@@ -1,1 +1,6 @@
 import axios from "axios";
+
+export const getGraphData = async () => {
+    const res = await axios.get('https://api.avantos-dev.io/docs#/operations/action-blueprint-graph-get');
+    return res.data;
+};
