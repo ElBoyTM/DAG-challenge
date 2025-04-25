aebccdd8dc4d9965fb374156b634523abdb5c428
ElBoyTM <nszalenski@gmail.com>
2025-04-25 15:23:42 -0400

    created basic graphview layout

--- app/src/components/Graph/GraphView.tsx
+++ app/src/components/Graph/GraphView.tsx
@@ -1,3 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import ReactFlow, { Background, Controls, Node, Edge } from 'reactflow';
 import 'reactflow/dist/style.css';
+
+const GraphView = () => {
+    return (
+        <div>
+            <ReactFlow>
+                <Background />
+                <Controls />
+            </ReactFlow>
+        </div>
+    );
+};
