aca90b5dc3714878f2610a759e8c877374b4ae07
ElBoyTM <nszalenski@gmail.com>
2025-04-26 14:18:35 -0400

    changed api get

--- app/src/services/api.ts
+++ app/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const getGraphData = async () => {
-    const res = await axios.get('http://localhost:3001/action-blueprint-graph-get');
+    const res = await axios.get('http://localhost:3001/api/v1/any/actions/blueprints/any/graph');
     return res.data;
 };
