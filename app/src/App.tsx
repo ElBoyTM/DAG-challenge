a5a7a730c3c273b4dc951e5967d579d685bcf051
ElBoyTM <nszalenski@gmail.com>
2025-04-27 14:19:52 -0400

    moved styling to CSS file

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,15 +1,10 @@
 import GraphView from './components/Graph/GraphView';
+import './App.css';
 
 function App() {
   return (
     <main style={{ padding: 0, position: 'relative' }}>
-      <h1 style={{
-        position: 'fixed',
-        top: '0',
-        left: '0',
-        margin: '10px',
-        zIndex: 10
-      }}>Journey Builder</h1>
+      <h1 className="app-title">Journey Builder</h1>
       <GraphView />
     </main>
   );
