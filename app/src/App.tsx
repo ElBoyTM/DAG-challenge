b3a60f7b5f5b2b72223067a12cf51876d6303607
ElBoyTM <nszalenski@gmail.com>
2025-04-27 14:11:15 -0400

    moved logo to top-left corner

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,8 +2,14 @@ import GraphView from './components/Graph/GraphView';
 
 function App() {
   return (
-    <main style={{ padding: 16 }}>
-      <h1>Journey Builder</h1>
+    <main style={{ padding: 0, position: 'relative' }}>
+      <h1 style={{
+        position: 'fixed',
+        top: '0',
+        left: '0',
+        margin: '10px',
+        zIndex: 10
+      }}>Journey Builder</h1>
       <GraphView />
     </main>
   );
